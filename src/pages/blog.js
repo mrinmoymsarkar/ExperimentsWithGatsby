e341a912df22e7afb37cd6bd0601cc94d6432fac
Mrinmoy Sarkar <msarkar07@live.in>
2019-07-29 13:10:02 +0530

    #1 feat:add routes and components

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import Navbar from "../components/Navbar"
+
+export default class blog extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div>Blog</div>
+      </div>
+    )
+  }
+}
